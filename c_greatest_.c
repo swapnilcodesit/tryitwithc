e5813d14fb4c9f97bbb09cd4f77cb050ffb2b231
swapnilcodesit <swapnillbhongale@gmail.com>
2021-03-11 10:26:27 +0530

    comments added
    comments added only at particular lines.

--- c_greatest_.c
+++ c_greatest_.c
@@ -1,12 +1,12 @@
 main()
 {
     int a,b;
-    printf("enter two numbers");
+    printf("enter two numbers");// takes two numbers
     scanf("%d%d",&a,&b);
     if(a>b)
-        printf("%d is greater number",a);
+        printf("%d is greater number",a);// if a is greater then
         else
-    printf("%d is greater number",b);
+    printf("%d is greater number",b);// else b is greater
     getch();
 
 }
