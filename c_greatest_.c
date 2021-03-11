2f6ee0a4dab621fab3f4f1a25a5d778f6193122c
swapnilcodesit <80399606+swapnilcodesit@users.noreply.github.com>
2021-03-11 10:03:30 +0530

    c file added
    only a c file of greatest no program has been added

---
+++ c_greatest_.c
@@ -0,0 +1,12 @@
+main()
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+        printf("%d is greater number",a);
+        else
+    printf("%d is greater number",b);
+    getch();
+
+}
