6c8caa2fe10df3d813468b98e53a2d26720aaff5
swapnilcodesit <swapnillbhongale@gmail.com>
2021-03-11 19:42:54 +0530

    some changes

--- c_greatest_.c
+++ c_greatest_.c
@@ -3,7 +3,7 @@ main()
     int a,b;
     printf("enter two numbers");// takes two numbers
     scanf("%d%d",&a,&b);
-    if(a>b)
+    if(a>b)// checking only
         printf("%d is greater number",a);// if a is greater then
         else
     printf("%d is greater number",b);// else b is greater
